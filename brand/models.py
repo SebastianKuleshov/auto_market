318fd4056049c465360293f982c946e95c59d705
Sebastian <sebaseba7775@gmail.com>
2025-05-09 18:55:06 +0300

    fixed translate

--- brand/models.py
+++ brand/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Марка')
+
+    class Meta:
+        verbose_name = 'Марка'
+        verbose_name_plural = 'Марки'
 
     def __str__(self):
         return self.name
