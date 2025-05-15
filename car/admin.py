9e116b37bcb0de1345766265a8f82e70e41bd5bc
Sebastian <sebaseba7775@gmail.com>
2025-05-15 19:05:39 +0300

    applied migration, fixed car model

--- car/admin.py
+++ car/admin.py
@@ -3,4 +3,9 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'milage', 'year', 'location', 'price')
+    search_fields = ('brand__name', 'model')
+    list_filter = ('year', 'brand')
+
+admin.site.register(models.Car, CarAdmin)
